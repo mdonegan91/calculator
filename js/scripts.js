91e1f6613b12326e4562610ff16439aef38be5c6
Vera weikel <vera.weikel@gmail.com>
2023-01-09 14:16:38 -0800

    It will update division on js file.
    Co-authored-by: Molly Donegan <mollyrdonegan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,10 @@ function multiply (number1, number2){
   return number1 * number2;
 }
 
+function divide (number1, number2){
+  return number1 / number2;
+}
+
 // user interface logic 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
@@ -18,3 +22,4 @@ const number2 = parseInt(prompt("Enter another number:"));
 window.alert("The sum of those two numbers: " + add(number1, number2));
 window.alert("The subtraction of those two numbers is: " + subtract(number1, number2));
 window.alert("Multiplying these two numbers results in: " + multiply(number1, number2));
+window.alert("Dividing these two numbers results in: " + divide(number1, number2));
