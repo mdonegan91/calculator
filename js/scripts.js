8d7ba839ccb946eff331cf3ea97e202cb81a25e5
Vera weikel <vera.weikel@gmail.com>
2023-01-09 14:13:13 -0800

    It will update subtraction on js file.
    Co-authored-by: Molly Donegan <mollyrdonegan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,15 @@
 function add(number1, number2){
   return number1 + number2;
 }
+
+function subtract( number1, number2){
+  return number1 - number2;
+}
+
 // user interface logic 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1, number2));
+window.alert("The sum of those two numbers: " + add(number1, number2));
+window.alert("The subtraction of those two numbers is: " + subtract(number1, number2));
+
